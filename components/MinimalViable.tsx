f09cb426661fd68f6674a9de9d2554ca4a2e135d
arligle <0774543002@163.com>
2024-03-03 12:16:00 +0800

    init

---
+++ components/MinimalViable.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { SimpleTreeItemWrapper } from "@/sortabletree";
+import { SortableTree } from "@/sortabletree/SortableTree";
+import { TreeItemComponentProps, TreeItems } from "@/sortabletree/types";
+import React from "react";
+import { useState } from "react";
+
+export const MinimalViable = () => {
+  const [items, setItems] = useState(initialViableMinimalData);
+  return (
+    <SortableTree
+      items={items}
+      onItemsChanged={setItems}
+      TreeItemComponent={MinimalTreeItemComponent}
+    />
+  );
+};
+type MinimalTreeItemData = {
+  value: string;
+};
+/*
+ * Here's the component that will render a single row of your tree
+ */
+const MinimalTreeItemComponent = React.forwardRef<
+  HTMLDivElement,
+  TreeItemComponentProps<MinimalTreeItemData>
+>((props, ref) => (
+  /* you could also use FolderTreeItemWrapper if you want to show vertical lines.  */
+  <SimpleTreeItemWrapper {...props} ref={ref}>
+    <div>{props.item.value}</div>
+  </SimpleTreeItemWrapper>
+));
+
+/*
+ * Configure the tree data.
+ */
+const initialViableMinimalData: TreeItems<MinimalTreeItemData> = [
+  {
+    id: '1',
+    value: 'Jane',
+    children: [
+      { id: '4', value: 'John' },
+      { id: '5', value: 'Sally' },
+    ],
+  },
+  { id: '2', value: 'Fred', children: [{ id: '6', value: 'Eugene' }] },
+  { id: '3', value: 'Helen', canHaveChildren: false },
+];
